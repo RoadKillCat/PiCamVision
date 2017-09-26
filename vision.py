61575487a0b34f4ce1b4d883f625a276c240022c
roadkillcat <joedominciddon@gmail.com>
2017-09-26 21:57:27 +0100

    update stuct

--- vision.py
+++ vision.py
@@ -2,39 +2,38 @@
 
 Support only for RPI camera module"""
 
-import picamera
-import picamera.array
 import numpy as np
-import matplotlib.pyplot as plt
-import math, time
+import math
 
 def getRawImg(x, y):
-	"""Returns numpy array of the raw camera data of size 1663x1232"""
-	x = int(32*math.ceil(x/32))
-	y = int(16*math.ceil(y/16))
-	with picamera.PiCamera() as camera:
-		camera.resolution = (x, y)
-		output = np.empty((y, x, 3), dtype="uint8")	
-		camera.capture(output, "rgb")
-		return output
+    """Returns numpy array of the raw camera data of size 1663x1232"""
+    import picamera
+    import picamera.array
+    x = int(32*math.ceil(x/32))
+    y = int(16*math.ceil(y/16))
+    with picamera.PiCamera() as camera:
+        camera.resolution = (x, y)
+        output = np.empty((y, x, 3), dtype="uint8") 
+        camera.capture(output, "rgb")
+        return output
 
 def convertToGreyscale(img):
-	"""Returns img conveted to greyscale"""
-	return np.dot(img, [0.2126, 0.7152, 0.0722])
+    """Returns img conveted to greyscale"""
+    return np.dot(img, [0.2126, 0.7152, 0.0722])
 
 def getKernel(size, sigma):
-	"""Returns Kernel matrix required for Gaussian Blur"""
-	bl = int((size-1)/2)
-	weightMatrix = [[(1/(2*math.pi*sigma**2)) * math.e ** ((-1*(x**2+y**2))/(2*sigma**2)) for x in range(-bl,bl+1)] for y in range(-bl,bl+1)]
-	weightSum = sum([sum(r) for r in weightMatrix])
-	return np.array([[v/weightSum for v in r] for r in weightMatrix])
+    """Returns Kernel matrix required for Gaussian Blur"""
+    bl = int((size-1)/2)
+    weightMatrix = [[(1/(2*math.pi*sigma**2)) * math.e ** ((-1*(x**2+y**2))/(2*sigma**2)) for x in range(-bl,bl+1)] for y in range(-bl,bl+1)]
+    weightSum = sum([sum(r) for r in weightMatrix])
+    return np.array([[v/weightSum for v in r] for r in weightMatrix])
 
 def gaussianBlur(img, kSize, kSigma):
-	"""Return numpy array of the greyscale img gaussian blurred with the size and sigma vals"""
-	kernel = getKernel(kSize, kSigma)
-	d = int((kSize-1)/2)
-	gaussian = np.zeros(img.shape, dtype="uint8")
-	for y in range(d, img.shape[0]-d):
-		for x in range(d, img.shape[1]-d):
-			gaussian[y][x] = np.sum(np.multiply(img[y-d:y+d+1, x-d:x+d+1], kernel))
-	return gaussian
+    """Return numpy array of the greyscale img gaussian blurred with the size and sigma vals"""
+    kernel = getKernel(kSize, kSigma)
+    d = int((kSize-1)/2)
+    gaussian = np.zeros(img.shape, dtype="uint8")
+    for y in range(d, img.shape[0]-d):
+        for x in range(d, img.shape[1]-d):
+            gaussian[y][x] = np.sum(np.multiply(img[y-d:y+d+1, x-d:x+d+1], kernel))
+    return gaussian
