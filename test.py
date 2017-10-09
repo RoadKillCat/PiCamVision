d5f48205ae950b5c9c68b295c67b695d52cc0f91
roadkillcat <joedominiciddon@gmail.com>
2017-10-09 19:14:03 +0000

    test.py started chess detection

--- test.py
+++ test.py
@@ -9,13 +9,31 @@ sbl = vision.sobel(blr)
 
 print(sys.argv[1], sys.argv[2])
 
-for r in range(sbl.shape[1]):
-    if np.sum(sbl[:, r]) > int(sys.argv[1]):
-        sbl[:, r] = np.ones(sbl.shape[0]) * 255
+boardCols =[]
+boardRows = []
+inRol = False
+inCol = False
 
-for c in range(sbl.shape[0]):
-    if np.sum(sbl[c]) > int(sys.argv[2]):
-        sbl[c] = np.ones(sbl.shape[1]) * 255
+for c in range(sbl.shape[1]):
+    if np.sum(sbl[:, c]) > int(sys.argv[1]):
+        if not inCol:
+            sbl[:, c] = np.ones(sbl.shape[0]) * 255
+            inCol = True
+            boardCols.append(c)
+    else:
+        inCol = False
+	
 
+for r in range(sbl.shape[0]):
+    if np.sum(sbl[r]) > int(sys.argv[2]):
+        if not inRow:
+            sbl[r] = np.ones(sbl.shape[1]) * 255
+            inRow = True
+            boardRows.append(r)
+    else:
+        inRow = False
+
+print(boardCols)
+print(boardRows)
 plt.imshow(sbl, cmap="gray")
 plt.show()
