5038a0d96a54577baa15790190ab9ae527b0b96f
roadkillcat <joedominiciddon@gmail.com>
2017-10-09 21:12:43 +0000

    piece recognition for all colours in test.py done

--- test.py
+++ test.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-chess = np.load("chess.npy")
+#chess = np.load("chess.npy")
+chess = vision.rawImg(480, 360)
+chess = vision.greyscale(chess)
 blr = vision.gaussianBlur(chess, 3, 1.5)
 sbl = vision.sobel(blr)
 
@@ -17,7 +19,7 @@ inCol = False
 for c in range(sbl.shape[1]):
     if np.sum(sbl[:, c]) > int(sys.argv[1]):
         if not inCol:
-            sbl[:, c] = np.ones(sbl.shape[0]) * 255
+            #sbl[:, c] = np.ones(sbl.shape[0]) * 255
             inCol = True
             boardCols.append(c)
     else:
@@ -27,7 +29,7 @@ for c in range(sbl.shape[1]):
 for r in range(sbl.shape[0]):
     if np.sum(sbl[r]) > int(sys.argv[2]):
         if not inRow:
-            sbl[r] = np.ones(sbl.shape[1]) * 255
+            #sbl[r] = np.ones(sbl.shape[1]) * 255
             inRow = True
             boardRows.append(r)
     else:
@@ -35,5 +37,31 @@ for r in range(sbl.shape[0]):
 
 print(boardCols)
 print(boardRows)
+
+leftEdge = boardCols[1]
+rightEdge = boardCols[-2]
+topEdge = boardRows[1]
+bottomEdge = boardRows[-2]
+
+sqrSize = int(((rightEdge-leftEdge) + (bottomEdge-topEdge))/16) #dividing by 2 to get average then 8 as 8 squares
+sqrBorder = 10
+
+rois = [[x for x in range(8)] for y in range(8)]
+
+for y in range(8):
+    for x in range(8):
+        roi = sbl[topEdge+(y*sqrSize)+sqrBorder:topEdge+((y+1)*sqrSize)-sqrBorder, leftEdge+(x*sqrSize)+sqrBorder:leftEdge+((x+1)*sqrSize)-sqrBorder] 
+        rois[y][x] = np.sum(roi)
+        sbl[topEdge+(y*sqrSize)+sqrBorder:topEdge+((y+1)*sqrSize)-sqrBorder, leftEdge+(x*sqrSize)+sqrBorder:leftEdge+((x+1)*sqrSize)-sqrBorder] = 120
+        #print(x, y, np.sum(roi))
+
+print("sqrSize", sqrSize)
+
+[print(''.join([str(int(c)).rjust(6) for c in r])) for r in rois]
+
+sbl[:, leftEdge] = 255
+sbl[:, rightEdge] = 255
+sbl[topEdge] = 255
+sbl[bottomEdge] = 255
 plt.imshow(sbl, cmap="gray")
 plt.show()
