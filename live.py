47b028d43fd6f7ab7a2fdbd44ff03776f579b744
RoadKillCat <joedominiciddon@gmail.com>
2018-05-27 19:22:39 +0100

    proper placement of preview

--- live.py
+++ live.py
@@ -1,13 +1,10 @@
-import picamera, time, subprocess
-s = 600
+import picamera,time,subprocess,sys
+prev_w = 600
+prev_h = int(prev_w * 3/4)
 with picamera.PiCamera() as c:
     c.resolution = (2592, 1944)
     r = c.start_preview()
     r.fullscreen = False
-    try:
-        w,h = map(int, subprocess.check_output('xrandr').split(b'\n')[-2].split()[0].split(b'x'))
-        r.window = (w-s,h-s,s,s)
-    except FileNotFoundError:
-        print('cannot get screen resolution so cannot position feed')
-    print('Press ENTER to exit')
-    input()
+    w,h = map(int, subprocess.check_output('xrandr').split(b'\n')[-2].split()[0].split(b'x'))
+    r.window = (w-prev_w, h-prev_h, prev_w, prev_h)
+    input('press ENTER to exit')
--- test.py
+++ test.py
@@ -7,7 +7,6 @@ grey = vision.greyscale(raw)
 blr  = vision.gaussian_blur(grey,3,1.5)
 sbl  = vision.sobel(grey)
 
-
 fig = plt.gcf()
 for i,a in enumerate(['raw','grey','blr','sbl']):
     ax = fig.add_subplot(2,2,i+1)
